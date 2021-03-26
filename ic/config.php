aa5b1ad651b1956753b4c6c12e2179a64a58fb0c
NivedhaSugumaran <43202131+NivedhaSugumaran@users.noreply.github.com>
2021-03-26 17:08:48 +0530

    Update config.php

--- ic/config.php
+++ ic/config.php
@@ -1,8 +1,8 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'mynewdb.cnxnwp9zmsnt.ap-south-1.rds.amazonaws.com');
-define('DB_USERNAME', 'master');
+define('DB_SERVER', 'ls-368e16b6922a409f3c8f6c38fa4222ae912f9d6d.ct5zdyhxsqlg.ap-south-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', '12345678');
 define('DB_NAME', 'inspirecloud');
 
