a66190d4bdb9a214956909bdc8b090b21ed12b35
NivedhaSugumaran <43202131+NivedhaSugumaran@users.noreply.github.com>
2021-03-26 19:41:07 +0530

    Update delete.php

--- ic/delete.php
+++ ic/delete.php
@@ -1,42 +1,29 @@
 <?php
-// Process delete operation after confirmation
+ob_start();
 if(isset($_POST["certid"]) && !empty($_POST["certid"])){
     // Include config file
     require_once "config.php";
-    
-    // Prepare a delete statement
-    $sql = "DELETE FROM certificate WHERE certid = ?";
-    
+      $sql = "DELETE FROM certificate WHERE certid = ?";  
     if($stmt = mysqli_prepare($link, $sql)){
         // Bind variables to the prepared statement as parameters
         mysqli_stmt_bind_param($stmt, "i", $param_id);
-        
-        // Set parameters
         $param_id = trim($_POST["certid"]);
-        
-        // Attempt to execute the prepared statement
         if(mysqli_stmt_execute($stmt)){
-            // Records deleted successfully. Redirect to landing page
             header("location: welcome.php");
             exit();
         } else{
             echo "Oops! Something went wrong. Please try again later.";
         }
     }
-     
-    // Close statement
     mysqli_stmt_close($stmt);
-    
-    // Close connection
-    mysqli_close($link);
+  mysqli_close($link);
 } else{
-    // Check existence of id parameter
     if(empty(trim($_GET["certid"]))){
-        // URL doesn't contain id parameter. Redirect to error page
         header("location: error.php");
         exit();
     }
 }
+ob_end_flush();
 ?>
 <!DOCTYPE html>
 <html lang="en">
